c3a8bd413553f4847668c244a918604b71a874ae
Steven Paalz <stevenpaalz@gmail.com>
2023-03-03 15:45:27 -0500

    start inheritance

---
+++ inheritance.js
@@ -0,0 +1,7 @@
+Function.prototype.inherits = function(SuperClass) {
+    function Surrogate() {};
+    Surrogate.prototype = SuperClass.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+}
+
